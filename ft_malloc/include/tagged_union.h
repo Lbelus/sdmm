92fd3390913b6620764606076690d37d5c5ca623
Lorris Bélus <lorrisbelus@Lorriss-MacBook-Pro.local>
2024-08-03 19:44:39 +0200

    [lbs] updates types

--- ft_malloc/include/tagged_union.h
+++ ft_malloc/include/tagged_union.h
@@ -9,8 +9,20 @@
 #define _TAG_ENUM_
 enum tag_enum
 {
+    TAG_BOOL,
     TAG_INT,
-    TAG_FLOAT,
+    TAG_INT8,
+    TAG_INT16,
+    TAG_INT32,
+    TAG_INT64,
+    TAG_UINT8,
+    TAG_UINT16,
+    TAG_UINT32,
+    TAG_UINT64,
+    TAG_INTPTR,
+    TAG_UINTPTR,
+    TAG_FLOAT32,
+    TAG_FLOAT64,
     TAG_STRING
 };
 typedef enum tag_enum tag_t;
@@ -23,9 +35,18 @@ struct tagged_value_s
     tag_t tag;
     union
     {
-        int     *i;
-        float   *f;
-        char    *s;
+        int        *_int_;
+        int8_t     *int8;
+        int16_t    *int16;
+        int32_t    *int32;
+        int64_t    *int64;
+        uint8_t    *uint8;
+        uint16_t   *uint16;
+        uint32_t   *uint32;
+        uint64_t   *uint64;
+        float      *float32;
+        double     *float64;
+        char       *string;
     } value;
 };
 typedef struct tagged_value_s tagged_value_t;
--- ft_malloc/src/safe_sdmm/safe_sdmm.c
+++ ft_malloc/src/safe_sdmm/safe_sdmm.c
@@ -8,16 +8,43 @@ tagged_value_t safe_sdmm_malloc(size_t size, tag_t tag)
     switch (tag)
     {
         case TAG_INT:
-            tagged_value.value.i = (int*)sdmm_malloc(size);
+            tagged_value.value._int_ = (int*)sdmm_malloc(size);
             break;
-        case TAG_FLOAT:
-            tagged_value.value.f = (float*)sdmm_malloc(size);
+        case TAG_INT8:
+            tagged_value.value.int8 = (int8_t*)sdmm_malloc(size);
+            break;
+        case TAG_INT16:
+            tagged_value.value.int16 = (int16_t*)sdmm_malloc(size);
+            break;
+        case TAG_INT32:
+            tagged_value.value.int32 = (int32_t*)sdmm_malloc(size);
+            break;
+        case TAG_INT64:
+            tagged_value.value.int64 = (int64_t*)sdmm_malloc(size);
+            break;
+        case TAG_UINT8:
+            tagged_value.value.uint8 = (uint8_t*)sdmm_malloc(size);
+            break;                                                        
+        case TAG_UINT16:
+            tagged_value.value.uint16 = (uint16_t*)sdmm_malloc(size);
+            break;
+        case TAG_UINT32:
+            tagged_value.value.uint32 = (uint32_t*)sdmm_malloc(size);
+            break;
+        case TAG_UINT64:
+            tagged_value.value.uint64 = (uint64_t*)sdmm_malloc(size);
+            break;
+        case TAG_FLOAT32:
+            tagged_value.value.float32 = (float*)sdmm_malloc(size);
+            break;
+        case TAG_FLOAT64:
+            tagged_value.value.float64 = (double*)sdmm_malloc(size);
             break;
         case TAG_STRING:
-            tagged_value.value.s = (char*)sdmm_malloc(size);
+            tagged_value.value.string = (char*)sdmm_malloc(size);
             break;
         default:
-            tagged_value.value.i = NULL;
+            tagged_value.value._int_ = NULL;
     }
     return tagged_value;
 }
